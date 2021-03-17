8a410adef6deb81dd1876b6442179b1d3ebbe6f0
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2021-03-17 05:03:04 -0400

    Add a docstring on Python example.

--- python/example.py
+++ python/example.py
@@ -4,4 +4,17 @@ An example package for testing.
 
 
 def mysum(a, b):
+    """
+    Add two numbers together.
+
+    Parameters
+    ----------
+    a, b : Number
+        Numbers to be added together.
+
+    Returns
+    -------
+    Number
+        The sum of the two inputs.
+    """
     return a + b
